9e6ab7f42f79c25db4d4d4baead5c8d9662867f7
zhiyang <wzy816@gmail.com>
2021-02-17 15:20:57 +0800

    Create 0912.cpp

---
+++ leetcode/0912.cpp
@@ -0,0 +1,43 @@
+/*
+Sort an Array
+
+Given an array of integers nums, sort the array in ascending order.
+*/
+
+// implement merge sort
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int l = nums.size();
+        helper(nums,0,l-1);
+        return nums;
+    }
+        
+    void helper(vector<int>& nums, int start, int end){
+        if(start >= end){
+            return;
+        }
+        int mid = (start+end)/2;
+        helper(nums, start, mid);
+        helper(nums, mid+1, end);
+        int i=start;
+        int j=mid+1;
+        vector<int> temp;
+        
+        while(i<=mid || j<=end){
+            if(j == end+1 || (i!= mid+1 && nums[i] <= nums[j])){
+                temp.push_back(nums[i]);
+                i++;                
+            }else{
+                temp.push_back(nums[j]);
+                j++;                
+            }
+        }
+        for(int i = 0;i<temp.size();i++){
+            nums[start+i] = temp[i];
+        }
+        return;
+    }
+};
+;
