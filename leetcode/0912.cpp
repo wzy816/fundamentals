9077b5e8b67cd175e39eb0132bc84bb0890286fe
zhiyang <wzy816@gmail.com>
2021-03-08 23:58:41 +0800

    add quick sort

--- leetcode/0912.cpp
+++ leetcode/0912.cpp
@@ -40,4 +40,41 @@ public:
         return;
     }
 };
-;
+
+// implement quick sort
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        if(nums.size()<=1){
+            return nums;
+        }
+        helper(nums,0,nums.size()-1);
+        return nums;
+    }
+    void helper(vector<int>& nums, int start, int end){
+        if(start < end){
+            int i = start,j=end, x = nums[start];
+            while(i<j){
+                while(i<j && nums[j] > x){
+                    j--;
+                }
+                if(i<j){
+                    nums[i] = nums[j];
+                    i++;
+                }
+                while(i<j && nums[i] < x){
+                    i++;
+                }
+                if(i<j){
+                    nums[j] = nums[i];
+                    j--;
+                }
+            }
+            // here i=j
+            nums[i] = x;
+            helper(nums,start,i-1);
+            helper(nums,i+1,end);
+        }
+    }
+};
+
