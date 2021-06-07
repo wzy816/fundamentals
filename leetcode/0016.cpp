793735cf6248dfe0a6485e56810b318a6aee7e7a
zhiyang <wzy816@gmail.com>
2021-06-07 11:30:59 +0800

    Update 0016.cpp

--- leetcode/0016.cpp
+++ leetcode/0016.cpp
@@ -12,7 +12,8 @@ public:
         int n = nums.size();
         int ans = nums[0]+nums[1]+nums[2];
         for(int i=0;i<n-2;++i){
-            int j = i+1,k=n-1;
+            // two pointer
+            int j=i+1,k=n-1;
             while(j < k){
                 int total = nums[i]+nums[j]+nums[k];
                 if(abs(total-target) < abs(ans-target)){
